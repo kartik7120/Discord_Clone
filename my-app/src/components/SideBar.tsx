0c3d5a0e451bb7d031f251d02f1dc9b6c3a51368
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-19 12:28:57 +0530

    Added basic discord navbar

--- my-app/src/components/SideBar.tsx
+++ my-app/src/components/SideBar.tsx
@@ -1,10 +1,19 @@
+import { SiDiscord } from "react-icons/si";
+import { FaCompass } from "react-icons/fa";
+import { BsPlus, BsFillLightningFill } from "react-icons/bs"
+
 function SideBar() {
     return <div className="sidebar">
-        <i>A</i>
-        <i>B</i>
-        <i>C</i>
-        <i>D</i>
-        <i>E</i>
+        <SidebarIcon icon={<SiDiscord size={32} color={"#5663F7"} />} />
+        <SidebarIcon icon={<BsPlus size={32} />} />
+        <SidebarIcon icon={<BsFillLightningFill size={20} />} />
+        <SidebarIcon icon={<FaCompass size="20" />} />
+    </div>
+}
+
+function SidebarIcon({ icon }: any) {
+    return <div className="sidebar-icon">
+        {icon}
     </div>
 }
 export default SideBar;
