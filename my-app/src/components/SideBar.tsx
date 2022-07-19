2dc381e0de491963a7f7bcf73663da04ec88cf0d
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-19 11:01:38 +0530

    Removed margin from body tag

---
+++ my-app/src/components/SideBar.tsx
@@ -0,0 +1,10 @@
+function SideBar() {
+    return <div className="sidebar">
+        <i>A</i>
+        <i>B</i>
+        <i>C</i>
+        <i>D</i>
+        <i>E</i>
+    </div>
+}
+export default SideBar;
