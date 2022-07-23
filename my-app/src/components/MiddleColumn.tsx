eb64c60a51a9aa8655bd7775931aab15150bfbbe
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-23 18:50:46 +0530

    add a simple text area in the middle column

--- my-app/src/components/MiddleColumn.tsx
+++ my-app/src/components/MiddleColumn.tsx
@@ -1,30 +1,54 @@
 import { createStyles } from "@mantine/core";
 import { Textarea } from "@mantine/core";
 import { ScrollArea } from "@mantine/core";
+import { useLocalStorage, getHotkeyHandler } from "@mantine/hooks";
+import React, { useState } from "react";
 const useStyles = createStyles((theme, _params, getRef) => ({
     middle_column_class: {
         backgroundColor: theme.colors.discord_palette[2],
         padding: "1em",
         position: "relative",
-        fontFamily: 'Nunito'
+        fontFamily: 'Nunito',
+        color: theme.colorScheme === "dark" ? "white" : "black"
     },
     TextAreaClass: {
         position: "absolute",
         bottom: 23,
         width: "90%",
+    },
+    listClass: {
+        textDecoration: "none",
+        listStyle: "none"
     }
 }))
 function MiddleColumn() {
     const { classes } = useStyles();
-
+    const [state, setState] = useState("");
+    const [message, setMessageState] = useLocalStorage({ key: "messages", defaultValue: [""] });
+    function handleChange(e: any) {
+        const message: string = e.target.value;
+        setState(message);
+    }
+    function handleMessageSubmit(e: any) {
+        setMessageState(function (oldMessages) {
+            return [...oldMessages, state];
+        })
+        setState("");
+    }
     return <div className={classes.middle_column_class}>
         <ScrollArea type="hover" style={{ height: "40rem" }}>
             <ol>
-                <li>I am a list 1</li>
+                {message.map((ele: string, index: number) => (
+                    <li key={Math.random() * index * 54239} className={classes.listClass}>{ele}</li>
+                ))}
             </ol>
         </ScrollArea>
         <form action="" method="get">
-            <Textarea className={classes.TextAreaClass} placeholder="Enter your message" autosize minRows={1} size={"md"} />
+            <Textarea className={classes.TextAreaClass} value={state} onChange={handleChange}
+                placeholder="Enter your message"
+                autosize minRows={1} size={"md"} onKeyDown={getHotkeyHandler([
+                    ["Enter", handleMessageSubmit]
+                ])} />
         </form>
     </div>
 }
