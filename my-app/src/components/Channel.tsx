38c04082c1e4affbb19f69c59f8d8d1cc3f7f9ea
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-26 12:17:56 +0530

    Changing styles of middle column

--- my-app/src/components/Channel.tsx
+++ my-app/src/components/Channel.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const useStyles = createStyles((theme, _params, getRef) => ({
     grid_wrapper: {
         display: "grid",
-        gridTemplateColumns: "minmax(0,1fr) minmax(0,2fr) minmax(0,1fr)",
+        gridTemplateColumns: "minmax(0,1fr) minmax(0,3fr) minmax(0,1fr)",
         width: "100%",
         height: "100vh"
     }
