073e26903dea4df0631d3b70e1a225cadcf95218
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-07-30 08:41:45 +0530

    Added a function in global namespace to connect to a namespace

--- my-app/src/globalImports.tsx
+++ my-app/src/globalImports.tsx
@@ -1,3 +1,12 @@
 import { io } from "socket.io-client";
 const socket = io("http://localhost:4000/");
+
+interface ioNamespace {
+    namespace: string
+}
+function connectNamespace(namespace: ioNamespace) {
+    const nsp = io(`http://localhost:4000/${namespace}`);
+    return nsp;
+}
 export default socket;
+export { connectNamespace };
