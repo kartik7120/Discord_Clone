912bdc04b0e3e6d3ca25016452d8be0306857743
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-08-06 10:54:06 +0530

    Added a global namespace to handle dynamic namespace

--- my-app/src/globalImports.tsx
+++ my-app/src/globalImports.tsx
@@ -1,12 +1,12 @@
 import { io } from "socket.io-client";
 import { createContext } from "react";
 // const socket = io("http://localhost:4000/");
-const serverSocket = io("http://localhost:4000/Server");
-const channelSocket = io("http://localhost:4000/channel");
+// const serverSocket = io("http://localhost:4000/Server");
+// const channelSocket = io("http://localhost:4000/channel");
 const socketContext: React.Context<any> = createContext(null);
 function connectNamespace(namespace: string) {
     const nsp = io(`http://localhost:4000/${namespace}`);
     return nsp;
 }
 // export default socket;
-export { connectNamespace, socketContext, serverSocket, channelSocket };
+export { connectNamespace, socketContext };
