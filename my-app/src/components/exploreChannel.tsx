25aed3ae1a5cd14898877a96afa0a9b8aa514bb5
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-08-23 09:22:59 +0530

    Added basic styles in explore component

--- my-app/src/components/exploreChannel.tsx
+++ my-app/src/components/exploreChannel.tsx
@@ -1,4 +1,16 @@
+import { createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme, _params, getRef) => ({
+    explore_wrapper: {
+        backgroundColor: theme.colorScheme === "dark" ? theme.colors.discord_palette[1] : theme.white,
+        width: "100%"
+    }
+}))
+
 function ExploreComponents() {
-    return <h1>I am explore component</h1>
+    const { classes } = useStyles();
+    return <div className={classes.explore_wrapper}>
+        <h1>I am explore component</h1>
+    </div>
 }
 export default ExploreComponents;
