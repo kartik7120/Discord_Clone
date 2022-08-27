19b823a55b985b2a4ef93a893ca4b96d4b8998a9
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-08-27 11:18:39 +0530

    added scroll area in the explore tab

--- my-app/src/components/exploreChannel.tsx
+++ my-app/src/components/exploreChannel.tsx
@@ -48,48 +48,54 @@ function ExploreComponents() {
     const theme = useMantineTheme();
     const { classes } = useStyles();
     const { isSuccess, data } = useQuery(["namespace", "explore"], fetchChannels)
-    return <div className={classes.explore_wrapper}>
-        <BackgroundImage style={{
-            height: "50%",
-        }} radius="md" src="https://i.postimg.cc/dtJ6dNhC/discord-explore-background.jpg">
-            <Container size="xl" style={{
-                height: "50%",
-                alignItems: "flex-start"
-            }} fluid>
-                <Center style={{ width: "100%", height: 240 }}>
-                    <div>
-                        <Text size="xl" color="violet"
-                            weight={600}>Find Your Community</Text>
-                        <Text size="xl" color="violet"
-                            weight={600}>From gaming to music , to leaning there's a place for you.</Text>
-                    </div>
-                </Center>
-            </Container>
-        </BackgroundImage>
-            <div className={classes.channel_container}>
-                {
-                    isSuccess ? data.map((ele: fetchChannel) => {
-                        return (
-                            <Card shadow="xl" p="lg" component="a" href="https:://www.google.com" target="_blank" radius="md" withBorder style={{
-                                width: "21em"
-                            }}>
-                                <Card.Section>
-                                    <Image src="https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
-                                        radius="md" alt="Image of the channel" width="100%" height={200} withPlaceholder
-                                    />
-                                </Card.Section>
-                                <Text size="lg" align="left" weight="bold" style={{ marginTop: "0.5em" }}>
-                                    {ele.channelName}
-                                </Text>
-                                <Text lineClamp={4} align="left" size="sm">
-                                    {ele.description}
-                                </Text>
-                            </Card>
-                        )
-                    }) : ""
-                }
-            </div>
-    </div >
+    return (
+        <ScrollArea type="hover" style={{ height: "100vh", width: "100%", padding: 0 }}>
+            <div className={classes.explore_wrapper}>
+                <BackgroundImage style={{
+                    height: "50%",
+                    width: "95%",
+                    margin:"0 auto"
+                }} radius="md" src="https://i.postimg.cc/dtJ6dNhC/discord-explore-background.jpg">
+                    <Container size="xl" style={{
+                        height: "50%",
+                        alignItems: "flex-start"
+                    }} fluid>
+                        <Center style={{ width: "100%", height: 240 }}>
+                            <div>
+                                <Text size="xl" color="violet"
+                                    weight={600}>Find Your Community</Text>
+                                <Text size="xl" color="violet"
+                                    weight={600}>From gaming to music , to leaning there's a place for you.</Text>
+                            </div>
+                        </Center>
+                    </Container>
+                </BackgroundImage>
+                <div className={classes.channel_container}>
+                    {
+                        isSuccess ? data.map((ele: fetchChannel) => {
+                            return (
+                                <Card shadow="xl" p="lg" component="a" href="https:://www.google.com" target="_blank" radius="md" withBorder style={{
+                                    width: "21em"
+                                }}>
+                                    <Card.Section>
+                                        <Image src="https://images.unsplash.com/photo-1511216335778-7cb8f49fa7a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                                            radius="md" alt="Image of the channel" width="100%" height={200} withPlaceholder
+                                        />
+                                    </Card.Section>
+                                    <Text size="lg" align="left" weight="bold" style={{ marginTop: "0.5em" }}>
+                                        {ele.channelName}
+                                    </Text>
+                                    <Text lineClamp={4} align="left" size="sm">
+                                        {ele.description}
+                                    </Text>
+                                </Card>
+                            )
+                        }) : ""
+                    }
+                </div>
+            </div >
+        </ScrollArea>
+    )
 
 }
 export default ExploreComponents;
