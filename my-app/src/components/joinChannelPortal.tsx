a95a04c34c4a9257146a43e8690339a8a5b0ce56
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-08-29 11:22:48 +0530

    Added navigate button to back to explore tab

--- my-app/src/components/joinChannelPortal.tsx
+++ my-app/src/components/joinChannelPortal.tsx
@@ -1,19 +1,24 @@
 import { createStyles, Text, useMantineTheme, Button } from "@mantine/core";
 import { AiOutlineArrowLeft } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
 const useStyles = createStyles((theme, _params, getRef) => ({
     portal_class: {
         backgroundColor: theme.colors.discord_palette[0],
         display: "flex",
         justifyContent: "space-between",
-        alignItems:"center",
-        height:"3em"
+        alignItems: "center",
+        height: "3em"
     }
 }))
 function JoinChannelPortal() {
+    const navigate = useNavigate();
     const { classes } = useStyles();
     const theme = useMantineTheme();
+    function handleBackClick() {
+        navigate(`/explore`, { replace: true })
+    }
     return <div className={classes.portal_class}>
-        <Button type="button" variant={theme.colorScheme === "dark" ? "filled" : "white"}>
+        <Button type="button" onClick={handleBackClick} variant={theme.colorScheme === "dark" ? "filled" : "white"}>
             <AiOutlineArrowLeft />Back
         </Button>
         <Text size="lg" align="center" color={theme.white}>
