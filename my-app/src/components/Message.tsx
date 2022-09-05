cb92a7801471085edd7b9a3213e10ff434d086aa
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-09-05 10:31:27 +0530

    added some line identation

--- my-app/src/components/Message.tsx
+++ my-app/src/components/Message.tsx
@@ -26,7 +26,8 @@ function Message(props: any) {
             <Avatar src={props?.message_bearer.picture} radius="lg" />
         </div>
         <div>
-            <Modal opened={opened} centered onClose={() => setOpened(false)} withCloseButton={true} title={<Title>User Profile</Title>}>
+            <Modal opened={opened} centered onClose={() => setOpened(false)} withCloseButton={true}
+                title={<Title>User Profile</Title>}>
                 <Button variant="filled" size="lg">Send friend request</Button>
             </Modal>
             <Text size="lg" className={classes.user_name_text} onClick={() => setOpened(true)}>
@@ -42,7 +43,8 @@ function Message(props: any) {
                     </Text> : props.category === "video" ?
                         <video autoPlay loop muted style={{ borderRadius: "0.5em" }} src={props.message_content} /> :
                         props.category === "video_file" ?
-                            <video controls loop muted style={{ borderRadius: "0.5em", width: "50%" }} src={props.message_content} /> :
+                            <video controls loop muted style={{ borderRadius: "0.5em", width: "50%" }}
+                                src={props.message_content} /> :
                             props.category === "audio_file" ?
                                 <audio src={props.message_content} controls muted style={{ borderRadius: "0.5em" }} />
                                 : <img alt="sticker" style={{ borderRadius: "0.5em", width: "50%" }} src={props.message_content} />
