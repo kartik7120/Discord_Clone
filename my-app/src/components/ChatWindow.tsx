bfca3f2e50bfce8e57a9d8fed5db760808a341a0
Kaartik Shukla <kaartikshukla7120@gmail.com>
2022-09-06 21:51:50 +0530

    Added discord logo in the middle

--- my-app/src/components/ChatWindow.tsx
+++ my-app/src/components/ChatWindow.tsx
@@ -1,8 +1,22 @@
 import ActiveNow from "./ActiveNow";
+import { BackgroundImage, useMantineTheme, createStyles, clsx } from "@mantine/core";
+import discord1 from "../images/discord_svg_white.svg";
+import discord2 from "../images/discord_svg_black.svg";
+const useStyles = createStyles((theme, _params, getRef) => ({
+    img_class: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    }
+}))
 function ChatWindow() {
+    const theme = useMantineTheme();
+    const { classes } = useStyles();
     return <>
-        <div className="chat-window">
-
+        <div className={clsx("chat-window", classes.img_class)}>
+            {theme.colorScheme === "dark" ?
+                <img src={discord1} alt="Discord white logo" />
+                : <img src={discord2} alt="Discord black logo" />}
         </div>
         <ActiveNow />
     </>
